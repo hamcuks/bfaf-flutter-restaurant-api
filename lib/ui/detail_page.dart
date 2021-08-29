ed7d41e05e472d43abfbba614f9815a19eaa735f
hamcuks <ivannurilham60@gmail.com>
2021-08-29 15:10:29 +0700

    add consumer

--- lib/ui/detail_page.dart
+++ lib/ui/detail_page.dart
@@ -1,6 +1,10 @@
 import 'package:dicoding_submission_restaurant_app_api/model/detail_arguments_model.dart';
+import 'package:dicoding_submission_restaurant_app_api/model/detail_restaurant_model.dart';
+import 'package:dicoding_submission_restaurant_app_api/network/api_service.dart';
+import 'package:dicoding_submission_restaurant_app_api/provider/detail_restaurant_provider.dart';
 import 'package:dicoding_submission_restaurant_app_api/widget/info_restaurant_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class DetailPage extends StatelessWidget {
   const DetailPage({
@@ -13,6 +17,9 @@ class DetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => ApiService().detailRestaurant(data.id),
+      ),
       appBar: AppBar(
         centerTitle: true,
         title: Text(
@@ -31,71 +38,99 @@ class DetailPage extends StatelessWidget {
       ),
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 22),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  width: double.infinity,
-                  height: 200,
-                  decoration: BoxDecoration(
-                    color: Colors.black87,
-                    borderRadius: BorderRadius.circular(8),
-                    image: DecorationImage(
-                      fit: BoxFit.fill,
-                      image: NetworkImage(
-                          'https://restaurant-api.dicoding.dev/images/large/15'),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 28,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Deskripsi Restoran',
-                      style:
-                          TextStyle(fontSize: 26, fontWeight: FontWeight.w700),
-                    ),
-                    SizedBox(
-                      height: 16,
-                    ),
-                    Text(
-                      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.',
-                      style: TextStyle(fontSize: 16, height: 1.5),
-                      maxLines: 6,
-                      textAlign: TextAlign.justify,
-                    ),
-                    SizedBox(
-                      height: 16,
-                    ),
-                    Row(
-                      children: [
-                        InfoRestaurant(
-                          icon: Icons.star,
-                          iconColor: Colors.orange,
-                          text: '4.5',
-                        ),
-                        SizedBox(
-                          width: 22,
-                        ),
-                        InfoRestaurant(
-                          icon: Icons.location_pin,
-                          iconColor: Colors.red,
-                          text: '4.5',
-                        ),
-                      ],
-                    )
-                  ],
-                )
-              ],
+          child: ChangeNotifierProvider<DetailRestaurantProvider>(
+            create: (_) =>
+                DetailRestaurantProvider(apiService: ApiService(), id: data.id),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 22),
+              child: Consumer<DetailRestaurantProvider>(
+                builder: (context, data, _) {
+                  if (data.state == ResultState.LOADING) {
+                    print(data.state);
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  } else if (data.state == ResultState.HAS_DATA) {
+                    print("STATE: ${data.state}");
+                    return _buildDetailRestaurant(
+                        data.detailRestaurantResult!.restaurant);
+                  } else if (data.state == ResultState.NO_DATA) {
+                    return Center(
+                      child: Text('${data.message}'),
+                    );
+                  } else {
+                    return Center(
+                      child: Text(''),
+                    );
+                  }
+                },
+              ),
             ),
           ),
         ),
       ),
+    );
+  }
+
+  Column _buildDetailRestaurant(final data) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          width: double.infinity,
+          height: 200,
+          decoration: BoxDecoration(
+            color: Colors.black87,
+            borderRadius: BorderRadius.circular(8),
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              image: NetworkImage(
+                  'https://restaurant-api.dicoding.dev/images/large/${data.pictureId}'),
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 28,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Deskripsi Restoran',
+              style: TextStyle(fontSize: 26, fontWeight: FontWeight.w700),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Text(
+              "${data.description}",
+              style: TextStyle(fontSize: 16, height: 1.5),
+              maxLines: 6,
+              textAlign: TextAlign.justify,
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Row(
+              children: [
+                InfoRestaurant(
+                  icon: Icons.star,
+                  iconColor: Colors.orange,
+                  text: "${data.rating}",
+                ),
+                SizedBox(
+                  width: 22,
+                ),
+                InfoRestaurant(
+                  icon: Icons.location_pin,
+                  iconColor: Colors.red,
+                  text: "${data.city}",
+                ),
+              ],
+            )
+          ],
+        )
+      ],
     );
   }
 }
