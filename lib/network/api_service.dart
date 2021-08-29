f651ca7f47b2f8fd6df20c8a24789d6dbd499fb4
hamcuks <ivannurilham60@gmail.com>
2021-08-29 19:14:41 +0700

    add func search

--- lib/network/api_service.dart
+++ lib/network/api_service.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:dicoding_submission_restaurant_app_api/model/detail_restaurant_model.dart';
 import 'package:dicoding_submission_restaurant_app_api/model/list_restaurant_model.dart';
+import 'package:dicoding_submission_restaurant_app_api/model/search_restaurant_model.dart';
 
 import 'package:http/http.dart' as http;
 
@@ -26,6 +27,18 @@ class ApiService {
       var jsonData = json.decode(response.body);
       print(jsonData);
       return DetailRestaurantModel.fromJson(jsonData);
+    } else {
+      throw Exception('Gagal memuat data');
+    }
+  }
+
+  Future<SearchRestaurantModel?> searchRestaurant(String text) async {
+    final response = await http.get(Uri.parse(_url + '/search?q=${text}'));
+
+    if (response.statusCode == 200) {
+      var jsonData = json.decode(response.body);
+      print(jsonData);
+      return SearchRestaurantModel.fromJson(jsonData);
     } else {
       throw Exception('Gagal memuat data');
     }
