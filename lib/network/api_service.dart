aad04ab3c10b9f5a76b02f20012822c968d8088a
hamcuks <ivannurilham60@gmail.com>
2021-08-29 15:11:34 +0700

    add fetch detail restaurant func

--- lib/network/api_service.dart
+++ lib/network/api_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:dicoding_submission_restaurant_app_api/model/detail_restaurant_model.dart';
 import 'package:dicoding_submission_restaurant_app_api/model/list_restaurant_model.dart';
 
 import 'package:http/http.dart' as http;
@@ -13,6 +14,18 @@ class ApiService {
     if (response.statusCode == 200) {
       var jsonData = json.decode(response.body);
       return ListRestaurantModel.fromJson(jsonData);
+    } else {
+      throw Exception('Gagal memuat data');
+    }
+  }
+
+  Future<DetailRestaurantModel?> detailRestaurant(String id) async {
+    final response = await http.get(Uri.parse(_url + '/detail/${id}'));
+
+    if (response.statusCode == 200) {
+      var jsonData = json.decode(response.body);
+      print(jsonData);
+      return DetailRestaurantModel.fromJson(jsonData);
     } else {
       throw Exception('Gagal memuat data');
     }
