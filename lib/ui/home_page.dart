556c6d7472b686268c2be610efef908305bdde8a
hamcuks <ivannurilham60@gmail.com>
2021-08-30 15:28:22 +0700

    fixing overflow when type a text

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -24,6 +24,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.only(
