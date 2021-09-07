61059a625d7d2b0029646bb8fad12dc72153e32a
hamcuks <ivannurilham60@gmail.com>
2021-09-07 08:40:43 +0700

    add pref provider

---
+++ lib/provider/preferences_provider.dart
@@ -0,0 +1,23 @@
+import 'package:dicoding_submission_restaurant_app_api/network/preferences_helper.dart';
+import 'package:flutter/foundation.dart';
+
+class PreferencesProvider extends ChangeNotifier {
+  final PreferencesHelper _prefHelper = PreferencesHelper();
+
+  PreferencesProvider() {
+    _getTheme();
+  }
+
+  bool _isDarkMode = false;
+  bool get isDarkMode => _isDarkMode;
+
+  void _getTheme() async {
+    _isDarkMode = await _prefHelper.isDarkMode;
+    notifyListeners();
+  }
+
+  set isDarkMode(bool value) {
+    _prefHelper.darkMode(value);
+    notifyListeners();
+  }
+}
