c5b90ffdb1ad4b869fe9542cd6de833b7e2f0e5a
hamcuks <ivannurilham60@gmail.com>
2021-09-09 00:19:07 +0700

    add dailyreminder provider

--- lib/provider/preferences_provider.dart
+++ lib/provider/preferences_provider.dart
@@ -1,3 +1,6 @@
+import 'package:android_alarm_manager/android_alarm_manager.dart';
+import 'package:dicoding_submission_restaurant_app_api/helper/background_service.dart';
+import 'package:dicoding_submission_restaurant_app_api/helper/date_time_helper.dart';
 import 'package:dicoding_submission_restaurant_app_api/network/preferences_helper.dart';
 import 'package:flutter/foundation.dart';
 
@@ -20,7 +23,28 @@ class PreferencesProvider extends ChangeNotifier {
     _isDarkMode = value;
     _prefHelper.darkMode(value);
     notifyListeners();
+  }
 
-    //_getTheme();
+  bool _isReminderActive = false;
+  bool get isReminderActive => _isReminderActive;
+
+  Future<bool> dailyReminderRestaurant(bool val) async {
+    _isReminderActive = val;
+    if (_isReminderActive) {
+      print('reminder aktif');
+      notifyListeners();
+      return await AndroidAlarmManager.periodic(
+        Duration(hours: 24),
+        1,
+        BackgroundService.callback,
+        startAt: DateTimeHelper.format(),
+        exact: true,
+        wakeup: true,
+      );
+    } else {
+      print('reminder disabled');
+      notifyListeners();
+      return await AndroidAlarmManager.cancel(1);
+    }
   }
 }
