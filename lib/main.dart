9923e232f4cbdc8d6107cf4675402b2eed0e47fe
hamcuks <ivannurilham60@gmail.com>
2021-09-09 00:18:38 +0700

    initialize background service and notif helper

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,32 @@
+import 'package:android_alarm_manager/android_alarm_manager.dart';
+import 'package:dicoding_submission_restaurant_app_api/helper/background_service.dart';
 import 'package:dicoding_submission_restaurant_app_api/model/detail_arguments_model.dart';
+import 'package:dicoding_submission_restaurant_app_api/navigation.dart';
 import 'package:dicoding_submission_restaurant_app_api/network/api_service.dart';
+import 'package:dicoding_submission_restaurant_app_api/network/notification_helper.dart';
 import 'package:dicoding_submission_restaurant_app_api/provider/favourite_provider.dart';
 import 'package:dicoding_submission_restaurant_app_api/provider/preferences_provider.dart';
 import 'package:dicoding_submission_restaurant_app_api/provider/restaurant_provider.dart';
 import 'package:dicoding_submission_restaurant_app_api/theme.dart';
 import 'package:dicoding_submission_restaurant_app_api/ui/detail_page.dart';
 import 'package:dicoding_submission_restaurant_app_api/ui/home_page.dart';
+import 'package:dicoding_submission_restaurant_app_api/ui/settings_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final NotificationHelper _notificationHelper = NotificationHelper();
+  final BackgroundService _service = BackgroundService();
+
+  _service.initializeIsolate();
+  await AndroidAlarmManager.initialize();
+  await _notificationHelper.initNotification(flutterLocalNotificationsPlugin);
   runApp(MyApp());
 }
 
@@ -26,6 +43,7 @@ class MyApp extends StatelessWidget {
         ),
         ChangeNotifierProvider(
           create: (_) => PreferencesProvider(),
+          child: SettingsPage(),
         )
       ],
       child: Consumer<PreferencesProvider>(
@@ -39,6 +57,7 @@ class MyApp extends StatelessWidget {
             cardColor: Color(0xFF434343),
           ),
           themeMode: data.isDarkMode ? ThemeMode.dark : ThemeMode.light,
+          navigatorKey: navigatorKey,
           initialRoute: '/',
           routes: {
             '/': (context) => HomePage(),
