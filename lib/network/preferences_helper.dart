60d2eb9456f17320e7a8ef18ff7e216202d4f312
hamcuks <ivannurilham60@gmail.com>
2021-09-10 10:25:33 +0700

    add daily reminder preferences

--- lib/network/preferences_helper.dart
+++ lib/network/preferences_helper.dart
@@ -5,14 +5,25 @@ class PreferencesHelper {
       SharedPreferences.getInstance();
 
   static const DARK_MODE = 'DARK_MODE';
+  static const DAILY_REMINDER = 'DAILY_REMINDER';
 
   Future<bool> get isDarkMode async {
     final prefs = await _sharedPreferences;
     return prefs.getBool(DARK_MODE) ?? false;
   }
 
-  void darkMode(bool value) async {
+  void setDarkMode(bool value) async {
     final prefs = await _sharedPreferences;
     prefs.setBool(DARK_MODE, value);
+  }
+
+  Future<bool> get isDailyReminder async {
+    final prefs = await _sharedPreferences;
+    return prefs.getBool(DAILY_REMINDER) ?? false;
+  }
+
+  void setDailyReminder(bool value) async {
+    final prefs = await _sharedPreferences;
+    prefs.setBool(DAILY_REMINDER, value);
   }
 }
