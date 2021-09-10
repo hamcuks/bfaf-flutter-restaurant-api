f9c6c31a611df42b7c81cd9b8a2f191323f6bece
hamcuks <ivannurilham60@gmail.com>
2021-09-10 10:24:23 +0700

    refactor

--- lib/navigation.dart
+++ lib/navigation.dart
@@ -1,9 +1,10 @@
+import 'package:dicoding_submission_restaurant_app_api/model/detail_arguments_model.dart';
 import 'package:flutter/cupertino.dart';
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 class Navigation {
-  static intentWithData({String? routeName, Object? args}) {
+  static intentWithData({String? routeName, DetailArguments? args}) {
     navigatorKey.currentState?.pushNamed(routeName!, arguments: args!);
   }
 
